eec8a52a080a3fe49ff6d477b4e84489dded721f
Naomi Liu <naomi@nliu.net>
2022-08-11 16:58:52 -0700

    add u128 impl for fnv1a hashes, abstract functions into a macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,58 @@
 #![no_std]
 
-const FNV_OFFSET_BASIS_32: u32 = 0x811c9dc5;
-const FNV_OFFSET_BASIS_64: u64 = 0xcbf29ce484222325;
+const FNV_OFFSET_BASIS_32:  u32  = 0x811c9dc5;
+const FNV_OFFSET_BASIS_64:  u64  = 0xcbf29ce484222325;
+const FNV_OFFSET_BASIS_128: u128 = 0x6c62272e07bb014262b821756295c58d;
 
-const FNV_PRIME_32: u32 = 0x01000193;
-const FNV_PRIME_64: u64 = 0x00000100000001B3;
+const FNV_PRIME_32:  u32  = 0x01000193;
+const FNV_PRIME_64:  u64  = 0x00000100000001B3;
+const FNV_PRIME_128: u128 = 0x0000000001000000000000000000013B;
 
-/// Computes 64-bits fnv1a hash of the given slice, or up-to limit if provided.
-/// If limit is zero or exceeds slice length, slice length is used instead.
-pub const fn fnv1a_hash_64(bytes: &[u8], limit: Option<usize>) -> u64 {
-    let mut hash = FNV_OFFSET_BASIS_64;
+macro_rules! fnv_hash_impl {
+    ($typ:ty, $size:literal, $fn_name:ident, $str_fn_name:ident, $offset:expr, $prime:expr) => {
+        #[doc = concat![
+            "Computes ",
+            stringify!($size),
+            "-bits fnv1a hash of the given slice, or up-to limit if provided. ",
+            "If limit is zero or exceeds slice length, slice length is used instead.",
+        ]]
+        pub const fn $fn_name(bytes: &[u8], limit: Option<usize>) -> $typ {
+            let prime = $prime;
 
-    let mut i = 0;
-    let len = match limit {
-        Some(v) => {
-            if v <= bytes.len() && v > 0 {
-                v
-            } else {
-                bytes.len()
+            let mut hash = $offset;
+            let mut i = 0;
+            let len = match limit {
+                Some(v) if 0 < v && v <= bytes.len() => {
+                    v
+                },
+                _ => {
+                    bytes.len()
+                }
+            };
+
+            while i < len {
+                hash ^= bytes[i] as $typ;
+                hash = hash.wrapping_mul(prime);
+                i += 1;
             }
+            hash
         }
-        None => bytes.len(),
-    };
 
-    while i < len {
-        hash ^= bytes[i] as u64;
-        hash = hash.wrapping_mul(FNV_PRIME_64);
-        i += 1;
+        #[doc = concat![
+            "Computes ",
+            stringify!($size),
+            "-bit fnv1a hash from a str."
+        ]]
+        #[inline(always)]
+        pub const fn $str_fn_name(input: &str) -> $typ {
+            $fn_name(input.as_bytes(), None)
+        }
     }
-    hash
 }
 
-/// Computes 32-bits fnv1a hash of the given slice, or up-to limit if provided.
-/// If limit is zero or exceeds slice length, slice length is used instead.
-pub const fn fnv1a_hash_32(bytes: &[u8], limit: Option<usize>) -> u32 {
-    let mut hash = FNV_OFFSET_BASIS_32;
-
-    let mut i = 0;
-    let len = match limit {
-        Some(v) => {
-            if v <= bytes.len() && v > 0 {
-                v
-            } else {
-                bytes.len()
-            }
-        }
-        None => bytes.len(),
-    };
-
-    while i < len {
-        hash ^= bytes[i] as u32;
-        hash = hash.wrapping_mul(FNV_PRIME_32);
-        i += 1;
-    }
-    hash
-}
+fnv_hash_impl!{u32,  32,  fnv1a_hash_32,  fnv1a_hash_str_32,  FNV_OFFSET_BASIS_32,  FNV_PRIME_32}
+fnv_hash_impl!{u64,  64,  fnv1a_hash_64,  fnv1a_hash_str_64,  FNV_OFFSET_BASIS_64,  FNV_PRIME_64}
+fnv_hash_impl!{u128, 128, fnv1a_hash_128, fnv1a_hash_str_128, FNV_OFFSET_BASIS_128, FNV_PRIME_128}
 
 /// Computes 32-bits fnv1a hash and XORs higher and lower 16-bits.
 /// This results in a 16-bits hash value.
@@ -66,18 +64,6 @@ pub const fn fnv1a_hash_16_xor(bytes: &[u8], limit: Option<usize>) -> u16 {
     let upper: u16 = u16::from_ne_bytes([bytes[0], bytes[1]]);
     let lower: u16 = u16::from_ne_bytes([bytes[2], bytes[3]]);
     upper ^ lower
-}
-
-/// Computes 64-bit fnv1a hash from a str.
-#[inline(always)]
-pub const fn fnv1a_hash_str_64(input: &str) -> u64 {
-    fnv1a_hash_64(input.as_bytes(), None)
-}
-
-/// Computes 32-bit fnv1a hash from a str.
-#[inline(always)]
-pub const fn fnv1a_hash_str_32(input: &str) -> u32 {
-    fnv1a_hash_32(input.as_bytes(), None)
 }
 
 /// Computes 16-bit fnv1a hash from a str using XOR folding.
